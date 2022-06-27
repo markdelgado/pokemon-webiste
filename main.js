4c4e3c3784497ccd443b71c5b35941076d00e084
Mark Delgado <markdelgado.md@hotmail.com>
2022-06-27 17:46:29 -0400

    added styles

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const pokemon = ['charmander', 'bulbasaur', 'squirtle']
+const pokemonIDs = ['001', '004', '007']
+
+// loop over every ID
+// create an HTML element
+//set the HTML element values
+//append HTML element to the DOM
+
+pokemonIDs.map((id) => {
+   
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`
+    console.log(`img url`, imgUrl )
+    let div = document.createElement('div')
+    div.setAttribute('class', "pokemon-card")
+    let img = document.createElement(`img`)
+    img.src = imgUrl
+    
+    div.append(img)
+    document.body.append(div)
+})
